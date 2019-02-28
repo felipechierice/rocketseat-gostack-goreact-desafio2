af8669fd4b834bd204cef3db761520fb22241a91
Felipe Chierice <felipe.chierice@hotmail.com>
2019-02-28 00:11:47 -0300

    working on

---
+++ src/components/CompareList/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Container, Repository } from './styles';
+
+const CompareList = ({ repositories }) => (
+  <Container>
+    {repositories.map(repo => (
+      <Repository>
+        <header>
+          <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+          <strong>{repo.name}</strong>
+          <small>{repo.owner.login}</small>
+        </header>
+
+        <ul>
+          <li>
+            {repo.stargazers_count}
+            {' '}
+            <small>stars</small>
+          </li>
+          <li>
+            {repo.forks_count}
+            {' '}
+            <small>forks</small>
+          </li>
+          <li>
+            {repo.open_issues_count}
+            {' '}
+            <small>issues</small>
+          </li>
+          <li>
+            {repo.pushed_at}
+            {' '}
+            <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
+  </Container>
+);
+
+export default CompareList;
